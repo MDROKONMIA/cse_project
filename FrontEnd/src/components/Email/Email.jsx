acc86371a53ed1f4b9d2d8544934f354392838b1
MDROKONMIA <miarokon2001@gmail.com>
2023-03-09 09:01:39 +0600

    09march2023

---
+++ FrontEnd/src/components/Email/Email.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from 'react'
+import { useParams } from 'react-router-dom'
+
+const Email = () => {
+    const params = useParams();
+    const { email } = params;
+    const [sender_Email, setSenderEmail] = useState("");
+    const [email_Subject, setEmailSubject] = useState("");
+    const [emailBody, setEmailBody] = useState("");
+
+   const sentEmail=(e)=>{
+       e.preventDefault();
+       const myForm = new FormData();
+
+       myForm.set('to', newPassword);
+       myForm.set('confirmPassword', confirmPassword);
+
+       dispatch(resetPassword(params.token, myForm));
+        
+   }
+
+    return (
+        <div class="w-full max-w-3xl m-auto">
+            <form class="bg-white shadow-md m-auto  w-4/5 rounded px-8 pt-6 pb-8 mb-4" onSubmit={sentEmail}>
+                <div class="mb-4">
+                    <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+                        From
+                    </label>
+                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" value={sender_Email} type={"email"} name={"sender_Email"} required placeholder="Enter your email address" onChange={(e) => setSenderEmail(e.target.value)} />
+                </div>
+                <div class="mb-4">
+                    <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+                        To
+                    </label>
+                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" value={email} placeholder="Enter your password" />
+                </div>
+                <div class="mb-6">
+                    <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+                        Subject
+                    </label>
+                    <input class="shadow appearance-none border  rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" value={email_Subject} type="text" placeholder="Enter a subject" onChange={(e) => setEmailSubject(e.target.value)} />
+                    {/* <p class="text-red-500 text-xs italic">Please choose a password.</p> */}
+                </div>
+                <div class="mb-6">
+                    <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+                        Message
+                    </label>
+                    <textarea class="shadow appearance-none border  rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="text" rows={'10'} required placeholder="Type your message" value={emailBody} onChange={(e) => setEmailBody(e.target.value)} />
+                    {/* <p class="text-red-500 text-xs italic">Please choose a password.</p> */}
+                </div>
+                <div class="flex items-center justify-center">
+                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 w-full px-4 rounded focus:outline-none focus:shadow-outline uppercase" type={'submit'}>
+                        send
+                    </button>
+                    {/* <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
+                        Forgot Password?
+                    </a> */}
+                </div>
+            </form>
+            <p class="text-center text-gray-500 text-xs">
+                &copy;2020 Acme Corp. All rights reserved.
+            </p>
+        </div>
+    )
+}
+
+export default Email
